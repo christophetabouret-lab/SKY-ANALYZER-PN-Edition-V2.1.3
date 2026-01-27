8308e876038bcca6129a2f9a343255eafa00f2e8
christophetabouret-lab <christophetabouret@gmail.com>
2026-01-27 14:26:12 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'sky-analyzer-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './logo_sky.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
