359158eb1b7a5ea63e0f158547c277343cb0d74e
christophetabouret-lab <christophetabouret@gmail.com>
2026-02-05 16:50:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,14 @@
 const CACHE_NAME = 'sky-analyzer-v1';
 const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './logo_sky.png'
+  'index.html',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
